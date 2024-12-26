e17bec2068e5e018af2016d96a300fd33d5a6aac
Anuradha Hariharan <anuradhahariharan@Anuradhas-MacBook-Air.local>
2024-12-26 19:41:42 +0530

    changes to indian currency

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -79,7 +79,7 @@ const Add = () => {
                 </div>
                 <div className="add-price flex-col">
                     <p>Product price</p>
-                    <input onChange={onChangeHandler} value={data.price} type="number" name='price' placeholder='$20' />
+                    <input onChange={onChangeHandler} value={data.price} type="number" name='price' placeholder='₹200' />
                 </div>
                 
             </div>
