dbe670c5361a92625b706318dac16dc235368615
Anuradha Hariharan <anuradhahariharan@Anuradhas-MacBook-Air.local>
2024-12-26 19:22:01 +0530

    added link to header view menu button

--- front-end/src/components/Header/Header.jsx
+++ front-end/src/components/Header/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>Choose from a diverse menu of delicious options, featuring everything from local favorites to international flavors. Whether you’re in the mood for a quick bite or a full-course meal, we’ve got something to satisfy every craving.</p>
-      <button>View Menu</button>
+      <a href="#explore-menu"><button>View Menu</button></a>
       </div>
 
     </div>
