ecd578d394dfcdefd89d72413c75464a98e7ea21
Anuradha Hariharan <anuradhahariharan@Anuradhas-MacBook-Air.local>
2024-12-27 00:31:49 +0530

    minor changes in backend

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -25,7 +25,7 @@ const placeOrder = async (req,res)=>{
             product_data:{
                 name:item.name,
             },
-            unit_amount:item.price
+            unit_amount:item.price*100
             },
             quantity:item.quantity
 
@@ -36,7 +36,7 @@ const placeOrder = async (req,res)=>{
             product_data:{
                 name:"Delivery charges"
             },
-            unit_amount: Math.round(2 * 100),
+            unit_amount: 2*100,
         },
         quantity:1
        })
